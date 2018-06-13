609cbb988a65662f3bc83d169124f86e1123b7bc
Arcane1986 <richkane1986@gmail.com>
2018-06-13 19:38:08 -0400

    Initials Completed

--- Projects/Initials.py
+++ Projects/Initials.py
@@ -1,12 +1,20 @@
 def initials():
-  name = input("What is your full name? ")
-  name1=name[:name.find(" ")]
-  name2=name[name.find(" ")+1:]
-  name_initials = (name1[0] + name2[0])
-  return name_initials.upper()
-
-def input_processer(name,name2):
-  """Test if there are additional spaces and if it is non-alpha"""
+  valid_input = False
+  while valid_input == False:
+    name = input("What is your full name?(name can only contain letters and spaces) ")
+    for char in name:
+      if char.isalpha() or char ==" ":
+        valid_input = True
+      else:
+        valid_input = False
+        break
+  name=name.strip()
+  name_list = [name for name in name.split(" ")]
+  initial_list = [name_part[0] for name_part in name_list]
+  final_string =""
+  for initial in initial_list:
+    final_string+=initial
+  return final_string.upper()
 
 def main():
   print(initials())
